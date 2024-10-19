3b3b610314e6dad07cd2222e170e6237e2b70aa9
Evan L <eluo2015@gmail.com>
2024-10-18 22:53:29 -0700

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -18,24 +18,16 @@ import { initializeApp } from "firebase/app";// TODO: Add SDKs for Firebase prod
 
 
 export default function App() {
-  // const firebaseConfig = {
-  //   apiKey: `${import.meta.env.API_KEY}`,
-  //   authDomain: `${import.meta.env.AUTH_DOMAIN}`,
-  //   projectId: `${import.meta.env.PROJECT_ID}`,
-  //   storageBucket: `${import.meta.env.STORAGE_BUCKET}`,
-  //   messagingSenderId: `${import.meta.env.MESSAGING_SENDER_ID}`,
-  //   appId: `${import.meta.env.APP_ID}`,
-  //   measurementId: `${import.meta.env.MEASUREMENT_ID}`
-  // };
   const firebaseConfig = {
-    apiKey: "AIzaSyCNCkueEOa_YsTg4AvREfKBOmyX0WNPf88",
-    authDomain: "calhacks-2024-d14cb.firebaseapp.com",
-    projectId: "calhacks-2024-d14cb",
-    storageBucket: "calhacks-2024-d14cb.appspot.com",
-    messagingSenderId: "997204657070",
-    appId: "1:997204657070:web:3be4f71b699796f4dcbe3f",
-    measurementId: "G-1ZYP2WF6PY"
+    apiKey: `${import.meta.env.VITE_API_KEY}`,
+    authDomain: `${import.meta.env.VITE_AUTH_DOMAIN}`,
+    projectId: `${import.meta.env.VITE_PROJECT_ID}`,
+    storageBucket: `${import.meta.env.VITE_STORAGE_BUCKET}`,
+    messagingSenderId: `${import.meta.env.VITE_MESSAGING_SENDER_ID}`,
+    appId: `${import.meta.env.VITE_APP_ID}`,
+    measurementId: `${import.meta.env.VITE_MEASUREMENT_ID}`
   };
+
   
   const app = initializeApp(firebaseConfig);
   return (

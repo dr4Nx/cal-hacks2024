2147ff5ea4bba03383bdd3772d2ca6263ccda10a
frawgmanman <76454426+frawgmanman@users.noreply.github.com>
2024-10-19 14:53:32 -0700

    login/register page prelim css

--- front-end/src/pages/RegisterForm.jsx
+++ front-end/src/pages/RegisterForm.jsx
@@ -40,11 +40,13 @@ const RegisterForm = ({setLoading}) => {
 
   return (
     <>
-      <h1 className="font-bold text-2xl text-center my-10">
+    <div className = "text-center">
+      <h1 className="font-bold font-poppins text-[32px]">
         Unlock your academic potential
       </h1>
-      <div className="bg-lightsage rounded shadow p-10 m-10 text-center">
-        <h3 className="font-bold">Enter your information</h3>
+     
+        <h3 className="font-figtree mt-1 mb-4">Enter your information</h3>
+        <div className ="font-figtree">
         <form onSubmit={submitLogin}>
           <input
             className="shadow border-none appearance-none border rounded w-full py-2 px-3 my-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -83,7 +85,9 @@ const RegisterForm = ({setLoading}) => {
             value="submit"
           />
         </form>
-      </div>
+        </div>
+        </div>
+
     </>
   );
 };

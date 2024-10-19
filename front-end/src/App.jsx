223dfce7037a04a8d2ac8d08af0c33df83d990e8
frawgmanman <76454426+frawgmanman@users.noreply.github.com>
2024-10-18 21:10:07 -0700

    update login
    heheheheheheh

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,26 @@
+import Navbar from "./components/Navbar/NavBar.jsx";
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./pages/index.jsx"
+import About from "./pages/about.jsx"
+import Login from "./pages/login.jsx"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+      
+    </Router>
+    
+    
+    </>
   )
 }

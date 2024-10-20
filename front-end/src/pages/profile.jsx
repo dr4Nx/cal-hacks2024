ee5ed6ecf6a8cf93236c51829788c9fc11c3bdef
frawgmanman <76454426+frawgmanman@users.noreply.github.com>
2024-10-19 18:00:56 -0700

    profile page preliminary

--- front-end/src/pages/profile.jsx
+++ front-end/src/pages/profile.jsx
@@ -1,61 +1,112 @@
 import { useState } from 'react';
 import Marquee from "react-fast-marquee";
-const Tabs =({text}) =>{
+const Tabs = ({ text, setPage, page }) => {
     return (
         <>
-        <div className ="text-center bg-opacity-20 hover:bg-lightsage text-white font-figtree w-full py-3">
-        <p>{text}</p>
-        </div>
+
+            <input type="button" className="text-center bg-opacity-20 hover:bg-lightsage text-white font-figtree w-full py-3" value={text} onClick={() => { setPage(page) }}>
+            </input>
+
+
         </>
     );
 }
-const Sidebar =({name, lastName}) =>{
-    return(
+const Sidebar = ({ name, lastName, setPage }) => {
+    return (
         <>
-            <div className ="fixed bg-darksage h-full w-1/4 pt-32 text-center">
-                <h2 className ="text-white font-bold text-[48px] font-poppins">{name + " "+ lastName}</h2>
-                <Tabs text="Profile"/>
-                <Tabs text="Requests Manager"/>
-                <Tabs text="Store"/>
+            <div className="fixed top-0 bg-darksage h-full w-1/4 pt-32 text-center">
+                <h2 className="text-white font-bold text-[48px] font-poppins">{name + " " + lastName}</h2>
+                <Tabs text="Profile" setPage={setPage} page={0} />
+                <Tabs text="Requests Manager" setPage={setPage} page={1} />
+                <Tabs text="Store" setPage={setPage} page={2} />
             </div>
         </>
     );
 
 }
-const TabProfile =() =>{
-    return(
+const TabProfile = () => {
+    return (
         <>
-        <div className = "float-right">
-        <div className ="bg-lightsage text-center mt-10 mr-[100px] p-5 w-[500px]"> {/*profile*/}
-            <p>hello</p>
-        </div>
-        <div className = "float-right"> {/*Tutoring*/}
+            <div className="ml-[25%] mt-[125px]">
+                <h1 className="text-center font-poppins font-bold text-[48px] mb-[20px]">Hi John!</h1>
+                <div className="flex justify-center items-start space-x-8">
+                    <div className="bg-lightsage rounded-lg drop-shadow-lg text-center p-5 w-5/12"> {/*stats*/}
+                        <h3 className="font-poppins font-bold text-[24px]">John Doe</h3>
+                        <div className="font-figtree">
+                            <p>Email</p>
+                            <p className="text-sage">johndo@berkeley.edu</p>
+                            <p>Username</p>
+                            <p className="text-sage">john-doe-21</p>
+                            <p>Education</p>
+                            <p className="text-sage">UC Berkeley - Bachelors in Bear Business</p>
+                            <p>Preferred meeting format</p>
+                            <p className="text-sage">Zoom, Google Meets, in the great wide world ~~</p>
+                        </div>
+                        <input type="button" value="Edit Profile" className="rounded-lg font-figtree bg-sage text-white p-2"></input>
+                    </div>
+                    <div className="flex flex-col space-y-8 w-5/12">
+                        <div className="bg-lightsage rounded-lg drop-shadow-lg text-center p-5 w-full"> {/*Tutoring*/}
+                            <h3 className="font-poppins font-bold text-[24px]">Tutoring</h3>
+                            <div className="font-figtree">
+                                <p className="font-bold">Subjects</p>
+                                <p className="text-sage">Calculus, Linear Algebra, Diffrential Equations, Bear Physics</p>
+                            </div>
+                        </div>
+                        <div className="bg-lightsage rounded-lg drop-shadow-lg text-center p-5 w-full"> {/*profile*/}
+                            <h3 className="font-poppins font-bold text-[24px]">Stats</h3>
 
-        </div>
-        <div> {/*stats*/}
 
-        </div>
-        </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     );
 }
 
+const TabRequests = () => {
+    return (
+        <>
+        </>
+    );
+}
+const TabStore = () => {
+    return (
+        <>
+        </>
+    );
+}
 const Profile = () => {
+    const [page, setPage] = useState(0);
+
+    const choosePage = () => {
+        switch (page) {
+            case 0:
+                return <TabProfile />
+            case 1:
+                return <TabRequests />;
+            case 2:
+                return <TabStore />
+        }
+
+    }
 
     return (
         <>
-        <div className = "float-right w-9/12 shadow font-figtree mt-[80px]">
-        <Marquee>
-            There are currently 2014 open tutoring requests and 10523 available tutors!
-                      
-        </Marquee>
-        </div>
-        <Sidebar name ="John" lastName ="Doe"/>
-        <TabProfile />
+            <div className="fixed top-0 right-0 w-9/12 shadow font-figtree ml-3/12 mt-[80px]">
+                <Marquee>
+                    There are currently 2014 open tutoring requests and 10523 available tutors!
+
+                </Marquee>
+            </div>
+            <Sidebar name="John" lastName="Doe" setPage={setPage} />
+            {choosePage()}
         </>
 
-           
-        
+
+
     );
 };
 
